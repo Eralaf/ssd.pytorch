e1c86ea84d44d43a193a462cdb9f7947164b4896
Florian Napierala <florian.napierala@gmail.com>
2019-09-16 17:52:20 +0200

    Adding comments on detection.py

--- layers/functions/detection.py
+++ layers/functions/detection.py
@@ -1,41 +1,65 @@
 import torch
+
 from torch.autograd import Function
-from ..box_utils import decode, nms
-from data import voc as cfg
+from ..box_utils    import decode, nms
+from data           import voc          as cfg
 
 
 class Detect(Function):
+
     """At test time, Detect is the final layer of SSD.  Decode location preds,
     apply non-maximum suppression to location predictions based on conf
     scores and threshold to a top_k number of output predictions for both
     confidence score and locations.
     """
-    def __init__(self, num_classes, bkg_label, top_k, conf_thresh, nms_thresh):
-        self.num_classes = num_classes
+    def __init__(self,
+                 num_classes, # num_classes -> dépend du dataset
+                 bkg_label,   # 0
+                 top_k,       # 200
+                 conf_thresh, # 0.01
+                 nms_thresh): # 0.45
+
+        self.num_classes      = num_classes
         self.background_label = bkg_label
-        self.top_k = top_k
+        self.top_k            = top_k
         # Parameters used in nms.
-        self.nms_thresh = nms_thresh
+        self.nms_thresh       = nms_thresh
+
         if nms_thresh <= 0:
             raise ValueError('nms_threshold must be non negative.')
-        self.conf_thresh = conf_thresh
-        self.variance = cfg['variance']
 
-    def forward(self, loc_data, conf_data, prior_data):
+        self.conf_thresh      = conf_thresh
+        self.variance         = cfg['variance']
+
+    def forward(self,
+                loc_data,
+                conf_data,
+                prior_data):
+
         """
         Args:
             loc_data: (tensor) Loc preds from loc layers
                 Shape: [batch,num_priors*4]
+
             conf_data: (tensor) Shape: Conf preds from conf layers
                 Shape: [batch*num_priors,num_classes]
+
             prior_data: (tensor) Prior boxes and variances from priorbox layers
                 Shape: [1,num_priors,4]
         """
-        num = loc_data.size(0)  # batch size
-        num_priors = prior_data.size(0)
-        output = torch.zeros(num, self.num_classes, self.top_k, 5)
-        conf_preds = conf_data.view(num, num_priors,
-                                    self.num_classes).transpose(2, 1)
+        num        = loc_data.size(0)   # batch size
+        num_priors = prior_data.size(0) # number of default/priors boxes
+        output     = torch.zeros(num, self.num_classes, self.top_k, 5)  # output : matrice de la taille de [batchSize                x
+                                                                        #                                   nombre de classe         x
+                                                                        #                                   nombre de boxes retenues x
+                                                                        #                                   (indice de confiance + localisation)]
+
+        conf_preds = conf_data.view(num,                                # batch size
+                                    num_priors,                         # nombre de defaults boxes
+                                    self.num_classes).transpose(2, 1)   # nombre de classe
+                                                                        # on inverse les dimensions 2 et 1
+                                                                        # pour chaque classe, on a les indices de confiance sur l'ensemble des priors
+                                                                        # (correspond à la forme de l'output)
 
         # Decode predictions into bboxes.
         for i in range(num):
