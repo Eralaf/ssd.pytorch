26f9fd3d606fc0290f0cf6db6737dfa58229818e
Florian Napierala <florian.napierala@gmail.com>
2019-09-16 17:32:11 +0200

    Change __init__.py to use personnal data loader

--- data/__init__.py
+++ data/__init__.py
@@ -1,10 +1,13 @@
-from .voc0712 import VOCDetection, VOCAnnotationTransform, VOC_CLASSES, VOC_ROOT
+from .voc0712           import VOCDetection, VOCAnnotationTransform, VOC_CLASSES, VOC_ROOT
+from .dataset_loader    import PersonnalDetection, PersonnalVOCAnnotationTransform, CLASSES, P_ROOT
+from .coco              import COCODetection, COCOAnnotationTransform, COCO_CLASSES, COCO_ROOT, get_label_map
+from .config            import *
 
-from .coco import COCODetection, COCOAnnotationTransform, COCO_CLASSES, COCO_ROOT, get_label_map
-from .config import *
+import numpy             as np
+
 import torch
 import cv2
-import numpy as np
+
 
 def detection_collate(batch):
     """Custom collate fn for dealing with batches of images that have a different
