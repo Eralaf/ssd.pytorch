4ff34fbdceff3851d4342d6d7a3f22f9a8dd35c8
Florian Napierala <florian.napierala@gmail.com>
2019-09-25 11:32:10 +0200

    just few changes - not important

--- layers/box_utils.py
+++ layers/box_utils.py
@@ -3,7 +3,7 @@ import torch
 
 
 def point_form(boxes):
-    """ Convert prior_boxes to (xmin, ymin, xmax, ymax)
+    """ Convert prior_boxes (cx, cy, w, h) to (xmin, ymin, xmax, ymax)
     representation for comparison to point form ground truth data.
     Args:
         boxes: (tensor) center-size default boxes from priorbox layers.
@@ -11,7 +11,7 @@ def point_form(boxes):
         boxes: (tensor) Converted xmin, ymin, xmax, ymax form of boxes.
     """
     return torch.cat((boxes[:, :2] - boxes[:, 2:]/2,     # xmin, ymin
-                     boxes[:, :2] + boxes[:, 2:]/2), 1)  # xmax, ymax
+                      boxes[:, :2] + boxes[:, 2:]/2), 1) # xmax, ymax
 
 
 def center_size(boxes):
@@ -23,13 +23,13 @@ def center_size(boxes):
         boxes: (tensor) Converted xmin, ymin, xmax, ymax form of boxes.
     """
     return torch.cat((boxes[:, 2:] + boxes[:, :2])/2,  # cx, cy
-                     boxes[:, 2:] - boxes[:, :2], 1)  # w, h
+                      boxes[:, 2:] - boxes[:, :2], 1)  # w, h
 
 
 def intersect(box_a, box_b):
     """ We resize both tensors to [A,B,2] without new malloc:
-    [A,2] -> [A,1,2] -> [A,B,2]
-    [B,2] -> [1,B,2] -> [A,B,2]
+    [A,2] ---unsqueeze(1)---> [A,1,2] ---expand---> [A,B,2]
+    [B,2] ---unsqueeze(0)---> [1,B,2] ---expand---> [A,B,2]
     Then we compute the area of intersect between box_a and box_b.
     Args:
       box_a: (tensor) bounding boxes, Shape: [A,4].
@@ -39,11 +39,15 @@ def intersect(box_a, box_b):
     """
     A = box_a.size(0)
     B = box_b.size(0)
+
     max_xy = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
                        box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
+
     min_xy = torch.max(box_a[:, :2].unsqueeze(1).expand(A, B, 2),
                        box_b[:, :2].unsqueeze(0).expand(A, B, 2))
+
     inter = torch.clamp((max_xy - min_xy), min=0)
+
     return inter[:, :, 0] * inter[:, :, 1]
 
 
@@ -69,47 +73,57 @@ def jaccard(box_a, box_b):
 
 
 def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
-    """Match each prior box with the ground truth box of the highest jaccard
+    """
+    Match each prior box with the ground truth box of the highest jaccard
     overlap, encode the bounding boxes, then return the matched indices
     corresponding to both confidence and location preds.
+
     Args:
-        threshold: (float) The overlap threshold used when mathing boxes.
-        truths: (tensor) Ground truth boxes, Shape: [num_obj, num_priors].
-        priors: (tensor) Prior boxes from priorbox layers, Shape: [n_priors,4].
-        variances: (tensor) Variances corresponding to each prior coord,
-            Shape: [num_priors, 4].
-        labels: (tensor) All the class labels for the image, Shape: [num_obj].
-        loc_t: (tensor) Tensor to be filled w/ endcoded location targets.
-        conf_t: (tensor) Tensor to be filled w/ matched indices for conf preds.
-        idx: (int) current batch index
+        - threshold: (float)  The overlap threshold used when mathing boxes.
+        - truths:    (tensor) Ground truth boxes,                               Shape: [num_obj,    num_priors].
+        - priors:    (tensor) Prior boxes from priorbox layers,                 Shape: [n_priors,   4].
+        - variances: (tensor) Variances corresponding to each prior coord,      Shape: [num_priors, 4].
+        - labels:    (tensor) All the class labels for the image,               Shape: [num_obj].
+        - loc_t:     (tensor) Tensor to be filled w/ endcoded location targets.
+        - conf_t:    (tensor) Tensor to be filled w/ matched indices for conf preds.
+        - idx:       (int)    Current batch index
+
     Return:
         The matched indices corresponding to 1)location and 2)confidence preds.
     """
-    # jaccard index
-    overlaps = jaccard(
-        truths,
-        point_form(priors)
-    )
-    # (Bipartite Matching)
+    # Jaccard index
+    overlaps = jaccard( truths,
+                        point_form(priors))
+
+    ### (Bipartite Matching)
+
     # [1,num_objects] best prior for each ground truth
-    best_prior_overlap, best_prior_idx = overlaps.max(1, keepdim=True)
+    best_prior_overlap, best_prior_idx = overlaps.max(1, keepdim = True)
+
     # [1,num_priors] best ground truth for each prior
-    best_truth_overlap, best_truth_idx = overlaps.max(0, keepdim=True)
+    best_truth_overlap, best_truth_idx = overlaps.max(0, keepdim = True)
+
     best_truth_idx.squeeze_(0)
     best_truth_overlap.squeeze_(0)
     best_prior_idx.squeeze_(1)
     best_prior_overlap.squeeze_(1)
-    best_truth_overlap.index_fill_(0, best_prior_idx, 2)  # ensure best prior
+    best_truth_overlap.index_fill_(0, best_prior_idx, 2) # ensure best prior
+
     # TODO refactor: index  best_prior_idx with long tensor
     # ensure every gt matches with its prior of max overlap
+
     for j in range(best_prior_idx.size(0)):
         best_truth_idx[best_prior_idx[j]] = j
-    matches = truths[best_truth_idx]          # Shape: [num_priors,4]
-    conf = labels[best_truth_idx] + 1         # Shape: [num_priors]
-    conf[best_truth_overlap < threshold] = 0  # label as background
+
+    matches = truths[best_truth_idx]     # Shape: [num_priors,4]
+    conf    = labels[best_truth_idx] + 1 # Shape: [num_priors]
+
+    conf[best_truth_overlap < threshold] = 0 # label as background
+
     loc = encode(matches, priors, variances)
-    loc_t[idx] = loc    # [num_priors,4] encoded offsets to learn
-    conf_t[idx] = conf  # [num_priors] top class label for each prior
+
+    loc_t[idx]  = loc   # [num_priors,4] encoded offsets to learn
+    conf_t[idx] = conf  # [num_priors]   top class label for each prior
 
 
 def encode(matched, priors, variances):
